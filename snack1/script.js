1ce5d2b855ff8139d17b3b7481dfa7f2b4c77feb
andrea-desantis <andrea.desantis290@gmail.com>
2025-11-13 00:41:41 +0100

    first commit

---
+++ snack1/script.js
@@ -0,0 +1,9 @@
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da 
+// corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo oppure in console la 
+// bici con peso minore.
+
+let bici = [lasWag, bmx, snow, rossa];
+
+const peso 
