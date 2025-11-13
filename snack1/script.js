968d9de455e19f83180479fabc7f53e503354cd1
andrea-desantis <andrea.desantis290@gmail.com>
2025-11-13 01:41:28 +0100

    snack1

--- snack1/script.js
+++ snack1/script.js
@@ -4,6 +4,26 @@
 // Stampare a schermo oppure in console la 
 // bici con peso minore.
 
-let bici = [lasWag, bmx, snow, rossa];
+let bici = [
+    { nome: "lasWag", peso: 6.9 },
+    { nome: "bmx", peso: 5.7 },
+    { nome: "snow", peso: 8.0 },
+    { nome: "rossa", peso: 9.1 }
+];
 
-const peso 
+function leggera(bici) {
+
+    let biciLeggera = bici [0];
+
+    for(let i = 0; i < bici.length; i++){
+        if(bici[i].peso < biciLeggera.peso){
+            biciLeggera = bici[i];
+        }
+    }
+
+    return biciLeggera;
+}
+
+
+const piuLeggera = leggera(bici);
+console.log(`La bici più leggera è la ${piuLeggera.nome} con peso di ${piuLeggera.peso} kg.`);
