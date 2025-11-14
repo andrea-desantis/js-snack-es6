2af1678db2d5767fdfd9067161753bf4cdbdd292
andrea-desantis <andrea.desantis290@gmail.com>
2025-11-14 11:58:05 +0100

    hh

---
+++ snack3/script.js
@@ -0,0 +1,18 @@
+// Scrivere una funzione che accetti tre argomenti, 
+// un array e due numeri (a più piccolo di b). 
+// La funzione ritornerà un nuovo array con i valori che hanno 
+// la posizione compresa tra i due numeri. 
+// Non usare i metodi di array di JS
+
+let a = prompt('inserisci un numero: ');
+let b = prompt('inserisci un altro numero: ');
+let array = [];
+
+function numeriCompresi(a, b, array){
+
+    let nuovoArray = [];
+
+    for( let i=0; i<array)
+
+}
+//hh
